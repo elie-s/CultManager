b9403c9725f4800aa79a478c2f0eeec63dacd9fb
Jay Sampat <jaydsampat@gmail.com>
2020-02-09 17:33:19 +0100

    Added Developer Mode for stress testing and interactable buildings

--- CultManagerTest/CultManager/Assets/Scripts/CultistSpawner.cs
+++ CultManagerTest/CultManager/Assets/Scripts/CultistSpawner.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI ;
 
 public class CultistSpawner : MonoBehaviour
 {
-    public GameObject pedestrianPrefab;
-    public int pedestriansToSpawn;
-    public Text fps, agents;
+    [Header("Cultist Components")]
+    public GameObject cultistPrefab;
+    public int cultistsToSpawn;
+    public List<GameObject> cultistList;
+
+    [Header("UI Components")]
+    public Text fps, agents, sliderAgents;
+    public GameObject devPanel;
+    public GameObject statPanel;
 
     void Start()
     {
@@ -16,24 +22,64 @@ public class CultistSpawner : MonoBehaviour
 
     private void Update()
     {
-        fps.text ="FPS = "+((int)(1.0f / Time.smoothDeltaTime)).ToString();
+        fps.text = "FPS = " + ((int)(1.0f / Time.smoothDeltaTime)).ToString();
     }
 
     IEnumerator Spawn()
     {
         int count = 0;
-        while (count < pedestriansToSpawn)
+        while (count < cultistsToSpawn)
         {
-            GameObject obj = Instantiate(pedestrianPrefab);
+            GameObject obj = Instantiate(cultistPrefab);
+            cultistList.Add(obj);
             Transform child = transform.GetChild(Random.Range(0, transform.childCount - 1));
             obj.GetComponent<WaypointNavigator>().currentWaypoint = child.GetComponent<Waypoint>();
             obj.transform.position = child.position;
-            
+
             yield return new WaitForEndOfFrame();
 
             count++;
             agents.text = "AGENTS = " + count.ToString();
         }
-        
+
     }
+
+    public void KillAll()
+    {
+        while (cultistList.Count > 0)
+        {
+            
+            Destroy(cultistList[cultistList.Count-1]); 
+
+            agents.text = "AGENTS = " + (cultistList.Count-1).ToString();
+            cultistList.RemoveAt(cultistList.Count - 1);
+        }
+    }
+
+    public void Respawn()
+    {
+        KillAll();
+        StartCoroutine(Spawn());
+    }
+
+    public void OnSliderChange(float value)
+    {
+        cultistsToSpawn = Mathf.RoundToInt((value * 250f));
+        sliderAgents.text = cultistsToSpawn.ToString();
+    }
+
+    public void EnableDevMode(bool x)
+    {
+        if (x)
+        {
+            devPanel.SetActive(true);
+            statPanel.SetActive(true);
+        }
+        else
+        {
+            devPanel.SetActive(false);
+            statPanel.SetActive(false);
+        }
+    }
+
 }
--- CultManagerTest/CultManager/Assets/Scripts/PanZoom.cs
+++ CultManagerTest/CultManager/Assets/Scripts/PanZoom.cs
@@ -2,6 +2,7 @@
 
 public class PanZoom : MonoBehaviour
 {
+    public bool restrictMovement;
     public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
     public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
     public Camera cam;
@@ -16,52 +17,56 @@ public class PanZoom : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!restrictMovement)
         {
-            touchStart = GetWorldPosition(groundZ);
-        }
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 direction = touchStart - GetWorldPosition(groundZ);
-            cam.transform.position += direction;
-        }
-
-        // If there are two touches on the device...
-        if (Input.touchCount == 2)
-        {
-            // Store both touches.
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            // Find the position in the previous frame of each touch.
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            // Find the magnitude of the vector (the distance) between the touches in each frame.
-            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-            // Find the difference in the distances between each frame.
-            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-            // If the camera is orthographic...
-            if (cam.orthographic)
+            if (Input.GetMouseButtonDown(0))
             {
-                // ... change the orthographic size based on the change in distance between the touches.
-                cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-
-                // Make sure the orthographic size never drops below zero.
-                cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+                touchStart = GetWorldPosition(groundZ);
             }
-            else
+            if (Input.GetMouseButton(0))
             {
-                // Otherwise change the field of view based on the change in distance between the touches.
-                cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+                Vector3 direction = touchStart - GetWorldPosition(groundZ);
+                cam.transform.position += direction;
+            }
 
-                // Clamp the field of view to make sure it's between 0 and 180.
-                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+            // If there are two touches on the device...
+            if (Input.touchCount == 2)
+            {
+                // Store both touches.
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                // Find the position in the previous frame of each touch.
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                // Find the magnitude of the vector (the distance) between the touches in each frame.
+                float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+                // Find the difference in the distances between each frame.
+                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+                // If the camera is orthographic...
+                if (cam.orthographic)
+                {
+                    // ... change the orthographic size based on the change in distance between the touches.
+                    cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                    // Make sure the orthographic size never drops below zero.
+                    cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+                }
+                else
+                {
+                    // Otherwise change the field of view based on the change in distance between the touches.
+                    cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                    // Clamp the field of view to make sure it's between 0 and 180.
+                    cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+                }
             }
         }
+        
     }
 
     
@@ -69,7 +74,7 @@ public class PanZoom : MonoBehaviour
     private Vector3 GetWorldPosition(float z)
     {
         Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        Plane ground = new Plane(transform.forward, new Vector3(0, 0, z));
         float distance;
         ground.Raycast(mousePos, out distance);
         return mousePos.GetPoint(distance);
