bf1d6f70620418ff47294895d5b5f48e164d6c72
Jay Sampat <jaydsampat@gmail.com>
2020-03-10 23:44:45 +0100

    Puzzle Interaction
    Testing basic interaction for Puzzle mini-game

---
+++ CultManagerBuild/Assets/1_Scripts/Systems/Recruitment/PuzzleManagerUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleManagerUI : MonoBehaviour
+{
+    public GameObject[] playerTokens;
+    public GameObject[] locations;
+    public GameObject tokenObject;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TokenSelection(int tokenID)
+    {
+        tokenObject = playerTokens[tokenID];
+    }
+
+    public void PlaceToken(int locationId)
+    {
+        tokenObject.transform.parent = locations[locationId].transform;
+        tokenObject.transform.position = locations[locationId].transform.position;
+    }
+}
