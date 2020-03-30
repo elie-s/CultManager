2267149d186bbd2da02972bd908eb81ec541899c
Elie <e.sarfati@rubika-edu.com>
2020-03-30 18:36:41 +0200

    Build Proto V1

--- CultManagerBuild/Assets/1_Scripts/Notifications/NotificationsManager.cs
+++ CultManagerBuild/Assets/1_Scripts/Notifications/NotificationsManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_ANDROID || UNITY_EDITOR
 using Unity.Notifications.Android;
+#endif
 
 namespace CultManager
 {
@@ -11,7 +13,9 @@ namespace CultManager
         [Header("Channel Settings")]
         [SerializeField] private string channelID = default;
         [SerializeField] private string channelName = default;
+#if UNITY_ANDROID || UNITY_EDITOR
         [SerializeField] private Importance channelImportance = default;
+#endif
         [SerializeField] private string channelDescription = default;
         [Header("Test Notification Settings")]
         [SerializeField] private string notificationTitle = default;
@@ -24,6 +28,7 @@ namespace CultManager
 
         private void CreateChannel()
         {
+#if UNITY_ANDROID || UNITY_EDITOR
             AndroidNotificationChannel channel = new AndroidNotificationChannel()
             {
                 Id = channelID,
@@ -34,10 +39,13 @@ namespace CultManager
 
             AndroidNotificationCenter.RegisterNotificationChannel(channel);
             debug.Log("Channel: \"" + channelName + "\" (" + channelID + ") registered.", DebugInstance.Importance.Mandatory);
+#endif
         }
 
         public Notification SendNotificationIn(int _minutes)
         {
+            int id = 0;
+#if UNITY_ANDROID || UNITY_EDITOR
             AndroidNotification notification = new AndroidNotification();
 
             notification.Title = notificationTitle;
@@ -45,9 +53,9 @@ namespace CultManager
             notification.FireTime = System.DateTime.Now.AddMinutes(_minutes);
             notification.Style = NotificationStyle.BigTextStyle;
 
-            int id = AndroidNotificationCenter.SendNotification(notification, channelID);
+            id = AndroidNotificationCenter.SendNotification(notification, channelID);
             debug.Log("Notification: \"" + notificationTitle + "\" ("+ id + ") registered on channel: " + channelID + ". Notification is scheduled at " + System.DateTime.Now.AddMinutes(_minutes), DebugInstance.Importance.Mandatory);
-
+#endif
             return new Notification(System.DateTime.Now.AddMinutes(_minutes), id);
         }
     }
