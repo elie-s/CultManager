0458ff106cf82191858cdde54aae581ba1f6b989
Elie <e.sarfati@rubika-edu.com>
2020-04-14 17:13:43 +0200

    Camera controller

---
+++ CultManagerReboot/Assets/1_Scripts/Camera/DataStructure/CameraTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultManager
+{
+    [System.Serializable]
+    public struct CameraTarget
+    {
+        public Vector2 position;
+        public float size;
+
+        public CameraTarget(Vector2 _position, float _size)
+        {
+            position = _position;
+            size = _size;
+        }
+
+        public CameraTarget(Camera _cam)
+        {
+            position = _cam.transform.localPosition;
+            size = _cam.orthographicSize;
+        }
+    }
+}
