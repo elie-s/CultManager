a69323595d396b8a00258eea44471aeac31febac
Elie <e.sarfati@rubika-edu.com>
2020-05-01 10:48:07 +0200

    Floating system

---
+++ CultManagerReboot/Assets/1_Scripts/Juicy/Floating/FloatingBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultManager
+{
+    public class FloatingBehaviour : MonoBehaviour
+    {
+        [SerializeField] private bool playOnStart = default;
+        [SerializeField] private bool loop = default;
+        [SerializeField] private bool playLocal = default;
+        [SerializeField] private Transform floatingObject = default;
+        [SerializeField, DrawScriptable] private FloatingSettings settings;
+
+        private Transform transformToUse => floatingObject ? floatingObject : transform;
+
+        private void Start()
+        {
+            if(playOnStart) Float();
+        }
+
+        public void Float()
+        {
+            StartCoroutine(FloatRoutine());
+        }
+
+        private IEnumerator FloatRoutine()
+        {
+            yield return playLocal ? settings.PlayLocal(transformToUse) : settings.Play(transformToUse);
+
+            if (loop) StartCoroutine(FloatRoutine());
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(transform.position, transform.position + Vector3.up * settings.span);
+        }
+
+    }
+}
---
+++ CultManagerReboot/Assets/1_Scripts/Juicy/Floating/ScriptableObjects/FloatingSettings.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultManager
+{
+    [CreateAssetMenu(menuName = "CultManager/Juicy/FloatingSettings")]
+    public class FloatingSettings : ScriptableObject
+    {
+        public float span = 1.0f;
+        [DrawScriptable] public AnimationSet set;
+
+
+        public IEnumerator Play(Transform _transform)
+        {
+            Iteration iteration = new Iteration(set.animationDuration/2.0f, set.curve);
+
+            Vector2 startPos = _transform.position;
+            Vector2 endPos = startPos + Vector2.up * span;
+
+            while (iteration.isBelowOne)
+            {
+                _transform.position = Vector2.Lerp(startPos, endPos, iteration.curveEvaluation);
+
+                yield return iteration.YieldIncrement();
+            }
+
+            _transform.position = endPos;
+            iteration = new Iteration(set.animationDuration, set.curve);
+
+            while (iteration.isBelowOne)
+            {
+                _transform.position = Vector2.Lerp(startPos, endPos, 1.0f - iteration.curveEvaluation);
+
+                yield return iteration.YieldIncrement();
+            }
+
+            _transform.position = startPos;
+        }
+
+        public IEnumerator PlayLocal(Transform _transform)
+        {
+            Iteration iteration = new Iteration(set.animationDuration / 2.0f, set.curve);
+
+            Vector2 startPos = _transform.localPosition;
+            Vector2 endPos = startPos + Vector2.up * span;
+
+            while (iteration.isBelowOne)
+            {
+                _transform.localPosition = Vector2.Lerp(startPos, endPos, iteration.curveEvaluation);
+
+                yield return iteration.YieldIncrement();
+            }
+
+            _transform.localPosition = endPos;
+            iteration = new Iteration(set.animationDuration, set.curve);
+
+            while (iteration.isBelowOne)
+            {
+                _transform.localPosition = Vector2.Lerp(startPos, endPos, 1.0f - iteration.curveEvaluation);
+
+                yield return iteration.YieldIncrement();
+            }
+
+            _transform.localPosition = startPos;
+        }
+    }
+}
