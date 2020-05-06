c950a0e80fa1bd048a6e1e4cd5c6bb8e304f9ac0
Elie <e.sarfati@rubika-edu.com>
2020-05-06 23:46:56 +0200

    New Islands Fix Sample

---
+++ CultManagerReboot/Assets/1_Scripts/Playtests/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using UnityEngine;
+
+public class EmailSender : MonoBehaviour
+{
+    [ContextMenu("Test")]
+    private void PrepareEmail()
+    {
+        MailMessage mail = new MailMessage();
+        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+        mail.From = new MailAddress("eliesarfati675@gmail.com");
+        mail.To.Add("e.sarfati@rubika-edu.com");
+        mail.Subject = "Test";
+        mail.Body = "test body";
+
+        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        SmtpServer.Port = 587;
+        SmtpServer.Credentials = new NetworkCredential("eliesarfati675@gmail.com", "59itafraS") as ICredentialsByHost;
+        SmtpServer.EnableSsl = true;
+        SmtpServer.Timeout = 20000;
+        SmtpServer.UseDefaultCredentials = false;
+        try
+        {
+            SmtpServer.Send(mail);
+        }
+        catch (SmtpException myEx)
+        {
+            Debug.Log("Expection: \n" + myEx.ToString());
+        }
+    }
+}
