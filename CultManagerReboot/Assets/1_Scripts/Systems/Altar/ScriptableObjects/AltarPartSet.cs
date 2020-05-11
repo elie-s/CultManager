b6908f67a55882494ad3882913ddd6fe19909848
Jay Sampat <jaydsampat@gmail.com>
2020-05-11 22:16:44 +0200

    WIP Altar Integration

---
+++ CultManagerReboot/Assets/1_Scripts/Systems/Altar/ScriptableObjects/AltarPartSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CultManager
+{
+    [CreateAssetMenu(menuName = "CultManager/Altar/AltarSet")]
+    public class AltarPartSet : ScriptableObject
+    {
+        public AltarPartData[] altarPartDatas;
+    }
+}
+
