5b3644129da7d3f37a59dfcbc976264059e3d83a
Elie <e.sarfati@rubika-edu.com>
2020-05-25 14:05:53 +0200

    [SA Puzzle] Fix reset puzzle

---
+++ StandAlonePuzzle/Assets/1_Scripts/Demons/ScriptableObjects/DemonSpritesSet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultManager
+{
+    [CreateAssetMenu(menuName = "CultManager/Demons/Sprites Set")]
+    public class DemonSpritesSet : ScriptableObject
+    {
+        public Sprite[] spawns;
+        public Sprite demon;
+        public Sprite[] spawnsID;
+        public Sprite demonID;
+
+        public Sprite SpawnSpriteFromAccuracy(float _accuracy)
+        {
+
+            for (int i = 0; i < spawns.Length; i++)
+            {
+                if (_accuracy <= i / (spawns.Length - 1)) return spawns[i];
+            }
+
+            return spawns[spawns.Length];
+        }
+
+        public Sprite SpawnIDFromAccuracy(float _accuracy)
+        {
+
+            for (int i = 0; i < spawnsID.Length; i++)
+            {
+                if (_accuracy <= i / (spawnsID.Length - 1)) return spawnsID[i];
+            }
+
+            return spawnsID[spawnsID.Length];
+        }
+    }
+}
